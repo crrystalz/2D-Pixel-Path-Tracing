0a15bab709da16f9a93cb70083077ad087012138
crrystalz <lichupatnaik@gmail.com>
2023-10-14 18:00:21 -0700

    extreme optimization, lots more objects can be placed

--- main.py
+++ main.py
@@ -30,19 +30,23 @@ light_icon_rect = pygame.Rect(UI_WIDTH // 4, 20, UI_WIDTH // 2, UI_WIDTH // 2)
 solid_icon_rect = pygame.Rect(UI_WIDTH // 4, 80, UI_WIDTH // 2, UI_WIDTH // 2)
 flashlight_icon_rect = pygame.Rect(UI_WIDTH // 4, 140, UI_WIDTH // 2, UI_WIDTH // 2)
 
+
 # Functions
 def calculate_brightness(distance, max_intensity, shadow_factor=1):
     return (max_intensity * shadow_factor) / (distance**2 + 1)
 
+
 def is_ray_obstructed(start, end, obstacle):
     dx = end[0] - start[0]
     dy = end[1] - start[1]
-    for t in range(1, int(max(abs(dx), abs(dy)))):
-        x = start[0] + int(dx * t // max(abs(dx), abs(dy)))
-        y = start[1] + int(dy * t // max(abs(dy), abs(dx)))
+    max_dist = int(max(abs(dx), abs(dy)))
+    for t in range(1, max_dist):
+        x = start[0] + dx * t // max_dist
+        y = start[1] + dy * t // max_dist
         if [x, y] == obstacle:
             return True
     return False
+
 
 def calculate_shadow_brightness(x, y, light_positions, solid_positions):
     shadow_brightness = 0
@@ -67,52 +71,39 @@ def calculate_shadow_brightness(x, y, light_positions, solid_positions):
         distance_to_sun, SUN_INTENSITY, sun_shadow_factor
     )
 
-    total_brightness = shadow_brightness + sun_brightness
-    return total_brightness
+    return shadow_brightness + sun_brightness
+
 
 def draw_lighting(light_positions, flashlight_pos, sun_pos, solid_positions):
     for x in range(GRID_WIDTH):
         for y in range(GRID_HEIGHT):
-            shadow_brightness = calculate_shadow_brightness(
-                x, y, light_positions, solid_positions
-            )
+            total_brightness = 0
 
-            distance_to_sun = math.sqrt((x - sun_pos[0]) ** 2 + (y - sun_pos[1]) ** 2)
-            sun_shadow_factor = 1
-            for solid_pos in solid_positions:
-                if is_ray_obstructed([x, y], sun_pos, solid_pos):
-                    sun_shadow_factor = max(0, 1 - SHADOW_INTENSITY / distance_to_sun)
-                    break
-            sun_brightness = calculate_brightness(
-                distance_to_sun, SUN_INTENSITY, sun_shadow_factor
-            )
+            # Existing light and sun calculations
+            if light_positions or sun_pos:
+                total_brightness += calculate_shadow_brightness(x, y, light_positions, solid_positions)
 
-            flashlight_brightness = 0
+            # Flashlight brightness calculations if flashlight_pos is provided
             if flashlight_pos:
-                distance_to_flashlight = math.sqrt(
-                    (x - flashlight_pos[0]) ** 2 + (y - flashlight_pos[1]) ** 2
-                )
+                flashlight_brightness = 0
+                distance_to_flashlight = math.sqrt((x - flashlight_pos[0]) ** 2 + (y - flashlight_pos[1]) ** 2)
                 flashlight_shadow_factor = 1
                 for solid_pos in solid_positions:
                     if is_ray_obstructed([x, y], flashlight_pos, solid_pos):
-                        flashlight_shadow_factor = max(
-                            0, 1 - SHADOW_INTENSITY / distance_to_flashlight
-                        )
+                        flashlight_shadow_factor = max(0, 1 - SHADOW_INTENSITY / distance_to_flashlight)
                         break
-                flashlight_brightness = calculate_brightness(
-                    distance_to_flashlight, LIGHT_INTENSITY, flashlight_shadow_factor
-                )
-
-            total_brightness = (
-                shadow_brightness + sun_brightness + flashlight_brightness
-            ) / 3
-
+                flashlight_brightness += calculate_brightness(distance_to_flashlight, LIGHT_INTENSITY, flashlight_shadow_factor)
+                total_brightness += flashlight_brightness
+            
             brightness = min(max(int(total_brightness), 0), 255)
+
+            current_color = win.get_at((x * PIXEL_SIZE + UI_WIDTH, y * PIXEL_SIZE))[:3]
+            combined_brightness = [min(a + b, 255) for a, b in zip(current_color, (brightness, brightness, brightness))]
 
             if [x, y] not in solid_positions:
                 pygame.draw.rect(
                     win,
-                    (brightness, brightness, brightness),
+                    combined_brightness,
                     (x * PIXEL_SIZE + UI_WIDTH, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE),
                 )
             else:
@@ -122,136 +113,70 @@ def draw_lighting(light_positions, flashlight_pos, sun_pos, solid_positions):
                     (x * PIXEL_SIZE + UI_WIDTH, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE),
                 )
 
+
 def draw_ui(selected_tool):
     pygame.draw.rect(win, (150, 150, 150), (0, 0, UI_WIDTH, PIXEL_SIZE * GRID_HEIGHT))
-    pygame.draw.circle(win, (255, 255, 0), (UI_WIDTH // 2, 30), 20)
-    pygame.draw.rect(
-        win, SOLID_COLOR, (UI_WIDTH // 4, 70, UI_WIDTH // 2, UI_WIDTH // 2)
-    )
-    pygame.draw.polygon(
-        win,
-        (255, 255, 0),
-        [(UI_WIDTH // 2, 145), (UI_WIDTH // 2 - 15, 160), (UI_WIDTH // 2 + 15, 160)],
-    )
-    if selected_tool == "light":
-        pygame.draw.circle(win, (255, 0, 0), (UI_WIDTH // 2, 30), 22, 2)
-    elif selected_tool == "solid":
-        pygame.draw.rect(
-            win, (255, 0, 0), (UI_WIDTH // 4, 70, UI_WIDTH // 2, UI_WIDTH // 2), 2
-        )
-    elif selected_tool == "flashlight":
-        pygame.draw.polygon(
-            win,
-            (255, 0, 0),
-            [
-                (UI_WIDTH // 2, 145),
-                (UI_WIDTH // 2 - 15, 160),
-                (UI_WIDTH // 2 + 15, 160),
-            ],
-            2,
-        )
+    # Draw Icons
+    pygame.draw.rect(win, (255, 255, 0) if selected_tool == 'light' else (255, 255, 255), light_icon_rect)
+    pygame.draw.rect(win, (0, 0, 255) if selected_tool == 'solid' else (255, 255, 255), solid_icon_rect)
+    pygame.draw.rect(win, (255, 165, 0) if selected_tool == 'flashlight' else (255, 255, 255), flashlight_icon_rect)
+    # Add text or icons...
+    win.blit(font.render("Light", True, (0, 0, 0)), (5, 20))
+    win.blit(font.render("Solid", True, (0, 0, 0)), (5, 80))
+    win.blit(font.render("Flashlight", True, (0, 0, 0)), (5, 140))
 
-# Main Menu Loop
-show_menu = True
-while show_menu:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    selected_tool = 'light'
+    flashlight_pos = None
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos
+                grid_x, grid_y = (mouse_x - UI_WIDTH) // PIXEL_SIZE, mouse_y // PIXEL_SIZE
+                if 0 <= mouse_x < UI_WIDTH:
+                    if light_icon_rect.collidepoint(mouse_x, mouse_y):
+                        selected_tool = 'light'
+                    elif solid_icon_rect.collidepoint(mouse_x, mouse_y):
+                        selected_tool = 'solid'
+                    elif flashlight_icon_rect.collidepoint(mouse_x, mouse_y):
+                        selected_tool = 'flashlight'
+                else:
+                    if selected_tool == 'light':
+                        light_positions.append([grid_x, grid_y])
+                    elif selected_tool == 'solid':
+                        solid_positions.append([grid_x, grid_y])
+                    elif selected_tool == 'flashlight':
+                        flashlight_pos = [grid_x, grid_y]
+
+        # Update flashlight position if the tool is selected
+        if selected_tool == 'flashlight':
+            flashlight_on = True
             mx, my = pygame.mouse.get_pos()
-            if run_button_rect.collidepoint(mx, my):
-                show_menu = False
+            flashlight_pos = [(mx - UI_WIDTH) // PIXEL_SIZE, my // PIXEL_SIZE]
+        else:
+            flashlight_on = False
+            flashlight_pos = []
 
-    # Clear the screen
-    win.fill((0, 0, 0))
+        win.fill((0, 0, 0))
 
-    # Draw title
-    title_text = font.render("2D Pixel Path Tracing Engine", True, (255, 255, 255))
-    title_rect = title_text.get_rect(center=(win.get_width() // 2, 100))
-    win.blit(title_text, title_rect)
+        # Drawing with pre-existing light positions
+        draw_lighting(light_positions, None, sun_pos, solid_positions)
+        # Overlaying flashlight if enabled
+        if flashlight_on:
+            draw_lighting([], flashlight_pos, [], solid_positions)
+            
+        draw_ui(selected_tool)
+        pygame.display.flip()
 
-    # Draw "Run" button
-    run_button = pygame.Rect(300, 300, 200, 50)
-    run_button_rect = pygame.draw.rect(win, (0, 255, 0), run_button)
-    button_text = font.render("Run", True, (0, 0, 0))
-    button_rect = button_text.get_rect(center=run_button_rect.center)
-    win.blit(button_text, button_rect)
+    pygame.quit()
 
-    pygame.display.flip()
 
-# Main Game Loop
-run_game = True
-selected_tool = None
-active_tool = None
-adding_new_objects = False
-flashlight_on = False
-flashlight_pos = []
-
-while run_game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run_game = False
-
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            adding_new_objects = False
-            active_tool = None
-            selected_tool = None
-
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            mx, my = pygame.mouse.get_pos()
-            if mx < UI_WIDTH:
-                if light_icon_rect.collidepoint(mx, my):
-                    active_tool = "light"
-                    selected_tool = (
-                        "light" if active_tool != selected_tool else None
-                    )
-                elif solid_icon_rect.collidepoint(mx, my):
-                    active_tool = "solid"
-                    selected_tool = (
-                        "solid" if active_tool != selected_tool else None
-                    )
-                elif flashlight_icon_rect.collidepoint(mx, my):
-                    active_tool = "flashlight"
-                    selected_tool = (
-                        "flashlight" if active_tool != selected_tool else None
-                    )
-                adding_new_objects = (
-                    active_tool is not None and active_tool == selected_tool
-                )
-
-            elif adding_new_objects and active_tool == "light":
-                grid_x = (mx - UI_WIDTH) // PIXEL_SIZE
-                grid_y = my // PIXEL_SIZE
-                if (
-                    [grid_x, grid_y] not in light_positions
-                    and [grid_x, grid_y] not in solid_positions
-                ):
-                    light_positions.append([grid_x, grid_y])
-
-            elif adding_new_objects and active_tool == "solid":
-                grid_x = (mx - UI_WIDTH) // PIXEL_SIZE
-                grid_y = my // PIXEL_SIZE
-                if (
-                    [grid_x, grid_y] not in light_positions
-                    and [grid_x, grid_y] not in solid_positions
-                ):
-                    solid_positions.append([grid_x, grid_y])
-
-    if selected_tool == "flashlight":
-        flashlight_on = True
-        mx, my = pygame.mouse.get_pos()
-        flashlight_pos = [(mx - UI_WIDTH) // PIXEL_SIZE, my // PIXEL_SIZE]
-    else:
-        flashlight_on = False
-        flashlight_pos = []
-
-    win.fill((0, 0, 0))
-    draw_lighting(
-        light_positions, flashlight_pos, sun_pos, solid_positions
-    )
-    draw_ui(selected_tool)
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
